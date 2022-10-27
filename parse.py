82d664117e1d45f176dff5a99c3d18138a4c23ae
Natalia Woodson <nwoodson@slack-corp.com>
2022-10-27 16:02:41 -0400

    correct count of tags and sort dict

--- parse.py
+++ parse.py
@@ -20,23 +20,29 @@ def parse_xml(xmlfile):
     root = tree.getroot()
 
     for child in list(root.iter()):
-        try:
-            if child.tag in tags:
-                tags[child.tag] += 1 
-            else:
-                tags[child.tag] = 1
-        except SyntaxError as e:
-            print(filename, e)
+        if child.tag == "canRoam":
+            print(xmlfile)
+        if child.tag in tags:
+            tags[child.tag] += 1 
+        else:
+            tags[child.tag] = 1
 
-        except TypeError as e:
-            print(filename, e)
 
-    
-    pprint.pprint(tags)
-
+count = 0
 for filename in glob.iglob('output_dir/jobs/**', recursive=True):
-    if os.path.isfile(filename) and filename.endswith('.xml'): # filter dirs
+    if os.path.isfile(filename) and filename.endswith('config.xml'): # filter dirs
+        count += 1
+    
         parse_xml(filename)
+
+
+
+
+print(f"Total num of config.xml files is: {count}")
+# pprint.pprint(tags)
+pprint.pprint(sorted(tags.items(), key=lambda item: item[1], reverse=True))
+
+ 
 
 
 
